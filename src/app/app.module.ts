cd2ad8105674f472d9cedafa5b80803984de9aea
oumaima benbarara <oumaimabenbarara@gmail.com>
2022-01-02 19:39:58 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule
